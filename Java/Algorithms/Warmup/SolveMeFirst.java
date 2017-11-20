55a9219f0c983d64a9cae9de78c2824b3b0445e7
amoy1110 <a.moy1110@gmail.com>
2017-11-20 17:07:40 -0500

    Update and rename solveMeFirst.java to SolveMeFirst.java

--- Java/Algorithms/Warmup/SolveMeFirst.java
+++ Java/Algorithms/Warmup/SolveMeFirst.java
@@ -6,13 +6,10 @@ import java.util.regex.*;
 
 public class Solution {
 
-
     static int solveMeFirst(int a, int b) {
         // Hint: Type return a+b; below
         return a+b;
-
     }
-
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
